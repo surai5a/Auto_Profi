466a49125cd039c5c9766712b7625d2e41189eb1
Surai5a <vadym.prymary@gmail.com>
2023-12-14 16:34:36 +0300

    Second

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+
+pymysql.version_info = (1, 4, 3, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
